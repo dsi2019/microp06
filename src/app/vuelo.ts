33b671e100664efd4f4056db386655d5697d0868
Sidd Srinivasan <siddsrinivasan@Sidds-MacBook-Pro.local>
2019-03-18 15:51:06 +0100

    pushing flight class

---
+++ src/app/vuelo.ts
@@ -0,0 +1,17 @@
+export class Vuelo {
+
+ public aerolínea:string;
+ public aerolina_foto:string;
+ public ida_inicio:string;
+ public ida_final:string;
+ public ida_inicio_hora:string;
+ public ida_final_hora:string;
+ public ida_fecha:Date;
+
+ public vuelta_inicio_hora:string;
+ public vuelta_final_hora:string;
+ public vuelta_fecha:Date;
+ 
+
+ public precio:number;
+}
