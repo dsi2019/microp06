b7c4686b9c0bd1641185976204d6e3201149857b
Christopher Obando <chrisjobando@gmail.com>
2019-03-18 16:19:31 +0100

    Datos class (info de pasajero)

--- src/app/vuelo.ts
+++ src/app/vuelo.ts
@@ -1,17 +1,15 @@
 export class Vuelo {
+    public aerolínea:string;
+    public aerolina_foto:string;
+    public ida_inicio:string;
+    public ida_final:string;
+    public ida_inicio_hora:string;
+    public ida_final_hora:string;
+    public ida_fecha:Date;
 
- public aerolínea:string;
- public aerolina_foto:string;
- public ida_inicio:string;
- public ida_final:string;
- public ida_inicio_hora:string;
- public ida_final_hora:string;
- public ida_fecha:Date;
+    public vuelta_inicio_hora:string;
+    public vuelta_final_hora:string;
+    public vuelta_fecha:Date;
 
- public vuelta_inicio_hora:string;
- public vuelta_final_hora:string;
- public vuelta_fecha:Date;
- 
-
- public precio:number;
+    public precio:number;
 }
