c3ca376a054de0cd293ec5d3eb1157e1d663f0fa
Sidd Srinivasan <siddsrinivasan@Sidds-MacBook-Pro.local>
2019-03-19 16:26:52 +0100

    más dummy vuelos

--- src/services/vuelo.service.ts
+++ src/services/vuelo.service.ts
@@ -13,7 +13,33 @@ export class VueloService{
         "vuelta_inicio_hora": "16:30",
         "vuelta_destino_hora": "18:30",
         "vuelta_fecha": new Date(2019,10,2),
-        "precio": 15
+        "precio": 150
+    },
+    {
+        "aerolínea":"British Airways",
+        "aerolínea_foto":"#",
+        "inicio":"Madrid",
+        "destino":"Paris",
+        "ida_inicio_hora":"12:30",
+        "ida_destino_hora":"14:30",
+        "ida_fecha": new Date​(​2019,​10​,​1​),
+        "vuelta_inicio_hora": "16:30",
+        "vuelta_destino_hora": "18:30",
+        "vuelta_fecha": new Date(2019,10,2),
+        "precio": 199
+    },
+    {
+        "aerolínea":"Ibería",
+        "aerolínea_foto":"#",
+        "inicio":"Madrid",
+        "destino":"Paris",
+        "ida_inicio_hora":"10:30",
+        "ida_destino_hora":"12:30",
+        "ida_fecha": new Date​(​2019,​10​,​1​),
+        "vuelta_inicio_hora": "12:30",
+        "vuelta_destino_hora": "15:30",
+        "vuelta_fecha": new Date(2019,10,2),
+        "precio": 300
     }];
     constructor(){
     }
