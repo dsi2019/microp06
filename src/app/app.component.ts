0834af3f19a0c2df20c57a8953bc6af55414cac2
raulvettel <33248213+raulvettel@users.noreply.github.com>
2019-03-24 12:20:41 +0100

    Creacion pagina check-in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { HomePage } from '../pages/home/home';
 import { ReservasPage } from '../pages/reservas/reservas';
 import { MisVuelosPage } from '../pages/mis-vuelos/mis-vuelos';
 import { TarjetasPage } from '../pages/tarjetas/tarjetas';
+import {CheckPage} from '../pages/checkin/checkin'; 
 import { InfoPage } from '../pages/info/info';
 import { WebPage } from '../pages/web/web';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { TarjetasPage } from '../pages/tarjetas/tarjetas';
 import {UnaTarjetaPage} from '../pages/una-tarjeta/una-tarjeta';
 import { InfoPage } from '../pages/info/info';
 import { WebPage } from '../pages/web/web';
-
+import { checkinPage } from '../pages/checkin/checkin';
 import {VueloService} from '../services/vuelo.service'
 
 @NgModule({
@@ -24,6 +24,7 @@ import {VueloService} from '../services/vuelo.service'
     MisVuelosPage,
     TarjetasPage,
     UnaTarjetaPage,
+	checkinPage,
     InfoPage,
     WebPage
   ],
@@ -39,6 +40,7 @@ import {VueloService} from '../services/vuelo.service'
     MisVuelosPage,
     TarjetasPage,
     UnaTarjetaPage,
+	checkinPage,
     InfoPage,
     WebPage
   ],
