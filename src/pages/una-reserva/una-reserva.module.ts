74d618df3f2d9281ab7421bf89f050fa60558a16
Christopher Obando <chrisjobando@gmail.com>
2019-03-24 20:42:24 +0100

    Added reservation button for found flights

---
+++ src/pages/una-reserva/una-reserva.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { UnaReservaPage } from './una-reserva';
+
+@NgModule({
+  declarations: [
+    UnaReservaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(UnaReservaPage),
+  ],
+})
+export class UnaReservaPageModule {}
